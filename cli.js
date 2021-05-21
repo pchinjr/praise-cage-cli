f30b0957c9eb15f8de331d83dd7427e7ba8fc3e0
Paul Chin Jr <pchinjr@gmail.com>
2021-05-21 16:20:19 -0400

    returns an image

---
+++ cli.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const tiny = require('tiny-json-http')
+
+var argv = require('yargs/yargs')(process.argv.slice(2))
+  .usage('Usage: $0 <cmd> [options]') // usage string of application.
+  .option('h', {
+    alias: 'help',
+    description: 'display help message'
+  })
+  .help('help')
+  .version('1.0.1', 'version', 'display version information') // the version string.
+  .alias('version', 'v')
+  .argv;
+
+console.log(argv);
+console.log('praise cage')
+getImage()
+
+async function getImage() {
+  const result = await tiny.get({url:'https://light-28e-staging.begin.app/images'})
+  console.log(result.body.image)
+}
